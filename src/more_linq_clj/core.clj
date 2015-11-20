96982d8bb9e4aca585c4b77efffc8af728a52eca
Volodymyr Prokopyuk <volodymrprokopyuk@gmail.com>
2015-11-20 16:35:21 +0100

    add series

--- src/more_linq_clj/core.clj
+++ src/more_linq_clj/core.clj
@@ -2,5 +2,9 @@
   (:require [ more-linq-clj.series :as series]))
 
 (defn xtest [ ]
-  (let [ item (series/dot-product) ]
+  ;(let [ item (series/dot-product) ]
+  ;(doseq [ item (series/pythagorean-triples) ]
+  ;(let [ item (series/weighted-sum) ]
+  ;(doseq [ item (series/percentile) ]
+  (doseq [ item (series/rank) ]
     (println item)))
--- src/more_linq_clj/series.clj
+++ src/more_linq_clj/series.clj
@@ -4,3 +4,27 @@
   (let [ v1 [ 1 2 3 ]
          v2 [ 3 2 1 ] ]
     (map * v1 v2)))
+
+(defn pythagorean-triples [ ]
+  (for [ c (range 2, 12) ]
+    { :length (* c 2) :height (- (* c c) 1) :hypotenuse (+ (* c c) 1) }))
+
+(defn weighted-sum [ ]
+  (let [ values [ 1 2 3 ]
+         weights [ 3 2 1 ] ]
+    (->> (map * values weights) (apply +))))
+
+(defn percentile [ ]
+  (let [ marks [ 20 15 31 34 35 40 50 90 99 100 ]
+         marks-count (count marks) ]
+    (for [ mark marks ]
+      (let [ lo-marks (filter #(< %1 mark) marks)
+             percentile (* (/ (count lo-marks) marks-count) 100) ]
+        { :mark mark :percentile percentile }))))
+
+(defn rank [ ]
+  (let [ percentiles (percentile)
+         rank-range (range 1, (+ (count percentiles) 1)) ]
+    (as-> percentiles $
+      (sort-by :percentile #(compare %2 %1) $)
+      (map #(assoc %1 :rank %2) $ rank-range))))
