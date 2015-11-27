2e20e32f369583f7034a40c5e0235cc14b485585
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2015-11-27 21:37:25 +0100

    add text

--- src/more_linq_clj/text.clj
+++ src/more_linq_clj/text.clj
@@ -11,10 +11,25 @@
    8 = TUV8
    9 = WXYZ9")
 
+(defn parse-t9 [ ]
+  (->> t9 str/lower-case (re-seq #"(\d) = (\w+)")
+    (mapcat (fn [ [ _ [ d ] ws ] ] (mapcat #(vector %1 d) ws)))
+    (apply hash-map)))
+
 (def dictionary
   #{ "gone" "good" "goof" "home" "hone" "hood" "hoof" "select" "reject"
      "textonym" "dictionary" "clojure" })
 
+(defn index-dictionary [ ]
+  (let [ char->digit (parse-t9) ]
+    (reduce
+      (fn [ dict word ]
+        (let [ hash (->> word (map char->digit) (apply str))
+               words (get dict hash [ ]) ]
+          (assoc dict hash (conj words word))))
+      { } dictionary)))
+
 (defn t9-dictionary [ ]
-  (let [ key "4663" ] ; "735328"
-    key))
+  (let [ key "4663" ; "735328"
+         dict (index-dictionary) ]
+    (get dict key)))
